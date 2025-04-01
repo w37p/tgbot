bd83b2ebfdf7757638cdf163df3f0d4453e249f0
root <root@server-pmcyvn.novalocal>
2025-04-01 17:31:07 +0000

    deepseek

--- main.go
+++ main.go
@@ -3,43 +3,49 @@ package main
 import (
 	"log"
 	"os"
-	"time"
 
-	"github.com/joho/godotenv"
-	"gopkg.in/telebot.v3"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func main() {
-	// Загружаем токен из .env
-	err := godotenv.Load()
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_BOT_TOKEN"))
 	if err != nil {
-		log.Fatal("Ошибка загрузки .env файла")
+		log.Panic(err)
 	}
 
-	token := os.Getenv("TELEGRAM_BOT_TOKEN")
-	if token == "" {
-		log.Fatal("Токен не найден, проверь .env файл")
+	bot.Debug = true
+
+	log.Printf("Бот запущен: %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		// Получаем имя пользователя
+		userName := getUserName(update.Message.From)
+
+		// Формируем ответ
+		reply := "Привет, " + userName + "! 😊"
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		if _, err := bot.Send(msg); err != nil {
+			log.Println("Ошибка отправки сообщения:", err)
+		}
 	}
+}
 
-	// Настройка бота
-	bot, err := telebot.NewBot(telebot.Settings{
-		Token:  token,
-		Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
-	})
-	if err != nil {
-		log.Fatal(err)
+// getUserName возвращает username или first_name, если username отсутствует
+func getUserName(user *tgbotapi.User) string {
+	if user.UserName != "" {
+		return "@" + user.UserName
 	}
-
-	// Обработчик команды /start
-	bot.Handle("/start", func(c telebot.Context) error {
-		return c.Send("Привет! Я ваш Telegram-бот на Go!")
-	})
-
-	// Обработчик любого текста
-	bot.Handle(telebot.OnText, func(c telebot.Context) error {
-		return c.Send("Привет!")
-	})
-
-	// Запуск бота
-	bot.Start()
+	return user.FirstName
 }
